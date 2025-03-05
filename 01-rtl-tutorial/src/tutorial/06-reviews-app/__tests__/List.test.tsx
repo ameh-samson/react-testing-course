267a3ed8bb32ebb95e289d3dd4f7f10468793fee
Samson Ameh <65107918+ameh-samson@users.noreply.github.com>
2025-03-05 16:01:57 +0100

    write test for list component

---
+++ 01-rtl-tutorial/src/tutorial/06-reviews-app/__tests__/List.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import List from "../List";
+import { Review } from "../Sandbox";
+
+const mockReviews: Review[] = [
+  { email: "test@example.com", rating: "4", text: "Great product!" },
+  { email: "user@example.com", rating: "4", text: "Excellent service!" },
+];
+
+describe("List Component", () => {
+  it("should render heading", () => {
+    render(<List reviews={[]} />);
+
+    expect(
+      screen.getByRole("heading", { level: 2, name: /reviews/i })
+    ).toBeInTheDocument();
+  });
+
+  it('displays "No reviews yet" when reviews array is empty', () => {
+    render(<List reviews={[]} />);
+
+    expect(screen.getByText(/no reviews yet/i)).toBeInTheDocument();
+  });
+
+  it("renders review correctly", () => {
+    render(<List reviews={mockReviews} />);
+
+    mockReviews.forEach((review) => {
+      expect(screen.getByText(review.email)).toBeInTheDocument();
+      expect(screen.getByText(review.text)).toBeInTheDocument();
+      const stars = "⭐".repeat(Number(review.rating));
+
+      screen.getAllByText(stars).forEach((star) => {
+        expect(star).toBeInTheDocument();
+      });
+    });
+  });
+});
