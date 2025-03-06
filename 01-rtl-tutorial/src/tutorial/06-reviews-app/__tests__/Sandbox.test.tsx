37040e9132729efebb0cd1eed4aa90671c8afe0f
Samson Ameh <65107918+ameh-samson@users.noreply.github.com>
2025-03-06 10:56:01 +0100

    integration test

---
+++ 01-rtl-tutorial/src/tutorial/06-reviews-app/__tests__/Sandbox.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen, logRoles } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Sandbox from "../Sandbox";
+import { getFormElements } from "./Form.test";
+
+describe("Reviews App", () => {
+  it("renders the review app title", () => {
+    render(<Sandbox />);
+
+    expect(
+      screen.getByRole("heading", { level: 1, name: /reviews app/i })
+    ).toBeInTheDocument();
+  });
+
+  it("add a new review when form is submitted", async () => {
+    const user = userEvent.setup();
+    render(<Sandbox />);
+    const { emailInput, ratingSelect, textArea, submitButton } =
+      getFormElements();
+
+    //   fill out the form
+    await user.type(emailInput, "test@example.com");
+    await user.selectOptions(ratingSelect, "5");
+    await user.type(textArea, "Great product!");
+    await user.click(submitButton);
+
+    expect(screen.getByText("test@example.com")).toBeInTheDocument();
+    expect(screen.getByText("Great product!")).toBeInTheDocument();
+    expect(screen.getByText("⭐".repeat(5))).toBeInTheDocument();
+  });
+});
